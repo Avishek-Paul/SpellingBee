580b514183318a4ba71edad435e7ccdb2a5ec4c2
Avishek Paul <avishek97paul@gmail.com>
2019-06-07 21:47:40 -0400

    usernames

--- public/javascript/roomLogic.js
+++ public/javascript/roomLogic.js
@@ -16,6 +16,22 @@ socket.on('synchWord', function(word){
   }
 })
 
+socket.on('playerList', (playerNames) => {
+  var playerList = document.getElementById("list");
+  var selfText = document.getElementById("self").innerHTML;
+  while ( playerList.firstChild ) {
+    playerList.removeChild( playerList.firstChild );
+  }
+  playerList.innerHTML = "<li id=\"self\">" + selfText + "</li>";
+  for(var i in playerNames){
+    var name = playerNames[i];
+    if(name == username) continue;
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(name));
+    playerList.appendChild(li)  
+  }
+});
+
 answerText.addEventListener("keypress", function(event){
   if(event.which==13 && answerText.value != "" && synchWord){
     if(synchWord == answerText.value.toLowerCase().trim()){
--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var session = require('client-sessions');
 
 app.disable('view cache');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })) 
+
+app.use(session({
+    cookieName: 'session',
+    secret: 'random_string_goes_here',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+  }));  
 
 const fs = require('fs');
 var lines;
@@ -20,24 +29,39 @@ app.get('/', function (req, res) {
     res.render('index')
 })
 
-app.get('/game/', function(req, res){
-    res.send('Enter url as: "/game/[GameID]" without the brackets([])')
+app.get('/game/:tagId', function(req, res){
+    
+    if (req.session && req.session.user) { // Check if session exists
+        res.render('game', {id: req.params.tagId, username: req.session.user})
+    } else {
+        res.redirect('/');
+    }
+
 })
 
-app.get('/game/:tagId', function(req, res){ 
-    res.render('game', {id: req.params.tagId})
-})
+app.post('/login', function(req, res) {
+    req.session.user = req.body.username;
+    res.redirect('game/' + req.body.room);
+});
 
 io.on('connection', function(socket){
-    var room = socket.handshake['query']['room']
+    // var room = socket.handshake['query']['room']
+    var room = socket.handshake.query.room;
+    socket.username = socket.handshake.query.username; //set socket with username based on session username
     socket.join(room)
-    console.log(socket.id, 'connected to room', room);
+    console.log(socket.username, 'connected to', room);
 
     if(!(room in roomDict)){
         roomDict[room] = lines[Math.floor(Math.random()*lines.length)];
     }
     io.to(socket.id).emit('synchWord', roomDict[room]);
-
+    
+    usernames = [];
+    for (var user in io.sockets.sockets){
+        usernames.push(io.sockets.sockets[user]['username'])
+    }
+    io.to(room).emit('playerList', usernames);
+    
     socket.on('requestWord', function(roomID){
         roomDict[roomID] = lines[Math.floor(Math.random()*lines.length)];
         io.to(roomID).emit('synchWord', roomDict[roomID]);
